43467d51f29998202aadb4b8f899548e7c45bc3d
Thor Summerall <thor.summerall@gmail.com>
2020-11-06 04:26:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,14 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 
+board = [" "," "," "," "," "," "," "," "," "]
+
 def display_board(board)
-  puts     |   |   
+  puts"  #{board[0]} | #{board[1]} | #{board[2]}  
  -----------
-    |   |   
+  #{board[3]} |#{board[4]}  | #{board[5]}   
  -----------
-    |   |   
+  #{board[6]} | #{board[7]} | #{board[8]}"
 end
+
+display_board(board)
